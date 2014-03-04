7260d3e5c3f22ee7af9514f009732fad24407bf8
Aaron Cohen <acohen@shutterstock.com>
2014-03-04 03:24:58 -0500

    Prepped for initial release
    - added documentation

--- cpanfile
+++ cpanfile
@@ -1,5 +1,7 @@
 requires 'Dancer';
 requires 'Dancer::Plugin';
+requires 'ExtUtils::MakeMaker';
+requires 'ExtUtils::MakeMaker::CPANfile';
 requires 'Scalar::Util';
 requires 'Set::Functional';
 requires 'Test::Most';
--- lib/Dancer/Plugin/Documentation.pm
+++ lib/Dancer/Plugin/Documentation.pm
@@ -1,5 +1,11 @@
 package Dancer::Plugin::Documentation;
 
+=head1 NAME
+
+Dancer::Plugin::Documentation - register documentation for a route
+
+=cut
+
 use strict;
 use warnings;
 
@@ -8,7 +14,57 @@ use Dancer::Plugin;
 use Scalar::Util (qw{blessed});
 use Set::Functional (qw{setify_by});
 
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Dancer::Plugin::Documentation provides the keyword I<documentation> to
+associate documentation with a fully pathed route.  This is especially
+useful when the route path is externally modified by the prefix command.
+See the example for recommended usage.
+
+Example usage:
+
+	package Foo;
+
+	use Dancer;
+	use Dancer::Plugin::Documentation;
+
+	get '/resources' => sub {
+		status 200;
+		return join "\n", Dancer::Plugin::Documentation->get_documentation;
+	};
+
+	prefix '/v1';
+
+	documentation 'A route to retrieve foo',
+	get '/foo' => sub { status 200; return 'foo' };
+
+	package main;
+
+	dance;
+
+=cut
+
 my %APP_TO_ROUTE_DOCUMENTATION;
+
+=head1 METHODS
+
+=cut
+
+=head2 documentation
+
+Given a documentation argument and a list of routes, associate the
+documentation with all of the routes.  The documentation argument
+can be anything from a string to a complex object.
+
+=cut
 
 register documentation => sub {
 	my ($documentation, @routes) = @_;
@@ -31,6 +87,15 @@ register documentation => sub {
 	return @routes;
 };
 
+=head2 get_documentation
+
+Retrieve the registered documentation for an app in lexicographical order by
+route, then method.  Defaults to the current app unless otherwise specified.
+Optionally, a route and/or method may be supplied to only show the
+corresponding documentation.
+
+=cut
+
 sub get_documentation {
 	my ($class, %args) = @_;
 	my ($app, $method, $route) = @args{qw{app method route}};
@@ -43,6 +108,13 @@ sub get_documentation {
 
 	return @docs;
 }
+
+=head2 set_documentation
+
+Register documentation for the method and route of a particular app.
+Documentation can be any defined value.
+
+=cut
 
 sub set_documentation {
 	my ($class, %args) = @_;
@@ -65,5 +137,79 @@ sub set_documentation {
 	return;
 }
 
+=head1 CAVEATS
+
+=over 4
+
+=item any
+
+The documentation keyword does not work with the I<any> keyword as it does not
+return the list of registered routes, but rather the number of routes
+registered.  Fixing this beahvior will require a patch to Dancer.
+
+=item get
+
+The I<get> keyword generates both get and head routes.  Documentation will be
+attached to both.
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+This module is brought to you by L<Shutterstock|http://www.shutterstock.com/>
+(L<@ShutterTech|https://twitter.com/ShutterTech>).  Additional open source
+projects from Shutterstock can be found at
+L<code.shutterstock.com|http://code.shutterstock.com/>.
+
+=head1 AUTHOR
+
+Aaron Cohen, C<< <aarondcohen at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-Dancer-Plugin-Documentation at rt.cpan.org>, or through
+the web interface at L<https://github.com/aarondcohen/Dancer-Plugin-Documentation/issues>.  I will
+be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::Documentation
+
+You can also look for information at:
+
+=over 4
+
+=item * Official GitHub Repo
+
+L<https://github.com/aarondcohen/Dancer-Plugin-Documentation>
+
+=item * GitHub's Issue Tracker (report bugs here)
+
+L<https://github.com/aarondcohen/Dancer-Plugin-Documentation/issues>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer-Plugin-Documentation>
+
+=item * Official CPAN Page
+
+L<http://search.cpan.org/dist/Dancer-Plugin-Documentation/>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Aaron Cohen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 register_plugin;
-1;
+1; # End of Dancer::Plugin::Documentation
